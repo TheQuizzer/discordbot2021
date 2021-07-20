3f662ebfbe08a2f0ee9a360bd1d02aab19300d3e
TheQuizzer <85128882+TheQuizzer@users.noreply.github.com>
2021-07-20 18:53:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at Port: ${port}`);
+});
+
+
 const Aoijs = require("aoi.js")
 
 const bot = new Aoijs.Bot({
